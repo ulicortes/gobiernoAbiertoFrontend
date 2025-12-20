a7ae0c1378907dd1f200ad6746d8eb91489d8db5
ulicortes <ulisestcortes@gmail.com>
2025-12-20 19:53:29 -0300

    Icono de uduario e informe de gestion agregados.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Footer from "@/components/Footer";
+import InfGestion from "@/components/InfGestion";
 import Nav from "@/components/Nav";
+import SessionIcon from "@/components/SessionIcon";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white font-sans dark:bg-black.">
       <Nav />
+      {/* <SessionIcon /> */}
+      <InfGestion />
       <Footer />
     </div>
   );
---
+++ src/components/InfGestion.tsx
@@ -0,0 +1,13 @@
+export default function InfGestion() {
+    return <div className="w-screen h-36 bg-green-500 rounded-tr-3xl rounded-bl-3xl flex-flex-col justify-center content-center">
+        <div className="w-full h-1/2 text-center text-3xl font-bold">
+            MIRA TODO LO QUE VENIMOS HACIENDO EN LOBERÌA
+        </div>
+        <div className="w-full h-1/2 flex flex-row justify-evenly">
+            <div className="w-1/6 h-2/3 bg-slate-200 rounded-md text-black text-center flex flex-col"><h1 className="text-2xl">2022</h1> <h1 className="text-sm">INFORME DE GESTION</h1></div>
+            <div className="w-1/6 h-2/3 bg-slate-200 rounded-md text-black text-center flex flex-col"><h1 className="text-2xl">2023</h1> <h1 className="text-sm">INFORME DE GESTION</h1></div>
+            <div className="w-1/6 h-2/3 bg-slate-200 rounded-md text-black text-center flex flex-col"><h1 className="text-2xl">2024</h1> <h1 className="text-sm">INFORME DE GESTION</h1></div>
+            <div className="w-1/6 h-2/3 bg-slate-200 rounded-md text-black text-center flex flex-col"><h1 className="text-2xl">2025</h1> <h1 className="text-sm">INFORME DE GESTION</h1></div>
+        </div>
+    </div>
+}
