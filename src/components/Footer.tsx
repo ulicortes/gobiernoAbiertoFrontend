d65853db8e748ebd72286013203b3498ead0c931
ulicortes <ulisestcortes@gmail.com>
2026-01-03 14:50:10 -0300

    Cambie el footer y agregue los logos de telefono y ubicacion.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,11 +7,18 @@ export default function Footer() {
             <div className="w-1/4 h-1 bg-yellow-base"></div>
         </div>
         <footer className="w-full h-32 bg-neutral-900 flex flex-row justify-evenly items-center">
-            <div className="w-1/3 h-full flex items-center justify-center items-center">
-                <img className="w-3/3" src="logo_municipio_footer.png" alt="" />
+            <div className="w-1/3 h-full flex items-center justify-center">
+                <img className="w-5/5" src="logo_municipio_footer.png" alt="" />
             </div>
-            <div className="w-1/3 h-full flex items-center justify-end">
-                <img className="w-2/3" src="footer_info.png" alt="" />
+            <div className="w-1/3 h-full flex flex-col items-center. justify-center.">
+                <div className="w-full h-1/2 flex flex-row justify-start items-center">
+                    <img className="w-6 h-5 px-1" src="Map.png" alt="" />
+                    <h1 className="text-sm">Avenida San Martín 51, Lobería, Buenos Aires</h1>
+                </div>
+                <div className="w-full h-1/2 flex flex-row justify-start items-center">
+                    <img className="w-6 h-5 px-1" src="Phone.png" alt="" />
+                    <h1 className="text-sm">02261 44-2126</h1>
+                </div>
             </div>
         </footer>
     </div>
